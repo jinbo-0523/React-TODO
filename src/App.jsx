0cee718d0105ab02daca093bf841ac49a438fc0f
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-05-06 00:19:08 +0900

    タスクの削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ export const App = () => {
     setIncompleteTodos(newTodos);
     setTodoText("");
   };
-
+  const onClickDelete = (index) => {
+    const newTodos = [...incompleteTodos];
+    // spliceによりｉｎｄｅｘに０が渡されたら、『0番目の配列の1個目を削除する』という言う処理が走る
+    newTodos.splice(index, 1);
+    setIncompleteTodos(newTodos);
+  };
   return (
     <>
       {/* jsxファイルはクラス付与にclassName=OOOと記述 */}
@@ -29,12 +34,12 @@ export const App = () => {
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
         <ul>
-          {incompleteTodos.map((todo) => {
+          {incompleteTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
